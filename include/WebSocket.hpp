4956ac6566e070127502d1b54323fc66025afa83
Ambroise Leclerc <ambroise.leclerc@physigenics.com>
2022-01-18 19:40:46 +0100

    websocket spawn

--- include/WebSocket.hpp
+++ include/WebSocket.hpp
@@ -1,7 +1,13 @@
 #pragma once
+#include <functional>
+#include <memory>
+#include <vector>
+
 
 namespace webfront {
 namespace websocket {
+
+using Handle = uint32_t;
 
 struct Header {
 	uint32_t FIN : 1;
@@ -15,6 +21,33 @@ struct Header {
 
 };
 static_assert(sizeof(Header) == 4, "WebSocket Header size must be 4 bytes");
+
+
+template<typename Socket>
+class WebSocket {
+	Socket socket;
+
+public:
+	WebSocket(Socket socket) : socket(std::move(socket)) {
+
+	}
+};
+
+template<typename Socket>
+class WSManager {
+	std::vector < std::shared_ptr<WebSocket<Socket>>> webSockets;
+	Handle handlesCounter = 0;
+public:
+	std::function<void(Handle)> onSpawn;
+
+public:
+	void createWebSocket(Socket socket) {
+		webSockets.push_back(std::make_shared<WebSocket<Socket>>(std::move(socket)));
+		handlesCounter = static_cast<Handle>(webSockets.size());
+		if (onSpawn) onSpawn(handlesCounter);
+	};
+};
+
 
 } // namespace websocket
 } // namespace webfront
--- src/WebFront.cpp
+++ src/WebFront.cpp
@@ -1,20 +1,26 @@
 ﻿
-#include <HTTPServer.hpp>
+#include <WebFront.hpp>
 
 #include <filesystem>
 #include <iostream>
 
-namespace net = std::experimental::net;
 
 int main() {
-    using namespace webfront;
-    using namespace webfront::http;
     using namespace std;
 
     cout << "WebFront launched from " << std::filesystem::current_path().string() << "\n";
 
     
-    Server server("0.0.0.0", "80");
-    server.run();
+    webfront::UI ui("80");
+    ui.runAsync();
+
+
+    while (true) {
+        std::string input;
+        cout << "> ";
+        cin >> input;
+        cout << input << "\n";
+    }
+
 
 }
