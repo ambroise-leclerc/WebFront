e146129a401c74353d3a77cf0b0708462df41ad8
Ambroise Leclerc <ambroise.leclerc@physigenics.com>
2022-02-02 08:32:43 +0100

    Missing files

---
+++ include/WebLink.hpp
@@ -0,0 +1,128 @@
+/// @date 31/01/2022 21:19:42
+/// @author Ambroise Leclerc
+/// @brief WebLink represents a connexion to an HTML/JS renderer
+#pragma once
+#include "tooling/Logger.hpp"
+#include "http/WebSocket.hpp"
+
+#include <cstddef>
+#include <optional>
+#include <span>
+#include <string_view>
+
+namespace webfront {
+
+using WebLinkId = uint16_t;
+
+struct WebLinkEvent {
+    enum class Code { linked, closed };
+    Code code;
+    WebLinkId webLinkId;
+};
+
+enum class JSEndian : uint8_t { little = 0, big = 1, mixed = little + big };
+
+enum class Command : uint8_t { handshake, ack, debugLog, textCommand };
+namespace msg {
+
+struct Ack {
+    std::span<const std::byte> header() { return std::span(reinterpret_cast<const std::byte*>(&head), sizeof(Header)); }
+    std::span<const std::byte> payload() { return {}; }
+
+private:
+    struct Header {
+        Command command = Command::ack;
+        JSEndian endian = std::endian::native == std::endian::little ? JSEndian::little : JSEndian::big;
+    } head;
+    static_assert(sizeof(Header) == 2, "Ack header has to be 2 bytes long");
+
+};
+
+struct DebugLog {
+    DebugLog(std::string_view text) : payloadSpan{std::span(reinterpret_cast<const std::byte*>(text.data()), text.size())} {
+        head.textLen = static_cast<uint16_t>(text.size());
+    }
+    std::span<const std::byte> header() { return std::span(reinterpret_cast<const std::byte*>(&head), sizeof(Header)); }
+    std::span<const std::byte> payload() { return payloadSpan; }
+
+private:
+    struct Header {
+        Command command = Command::debugLog;
+        std::array<uint8_t, 3> padding = {};
+        uint16_t textLen;
+    } head;
+    static_assert(sizeof(Header) == 6, "DebugLog header has to be 6 bytes long");
+
+    std::span<const std::byte> payloadSpan;
+};
+
+/// Commands with a text payload
+struct TextCommand {
+    enum class Opcode : uint8_t { debugLog, injectScript };
+    TextCommand(std::string_view text, Opcode opcode)
+        : head{.opcode = opcode, .lengthHi = static_cast<uint8_t>(text.size() >> 8), .lengthLo = static_cast<uint8_t>(text.size())},
+          payloadSpan{std::span(reinterpret_cast<const std::byte*>(text.data()), text.size())} {}
+
+    std::span<const std::byte> header() { return std::span(reinterpret_cast<const std::byte*>(&head), sizeof(Header)); }
+    std::span<const std::byte> payload() { return payloadSpan; }
+private:
+    struct Header {
+        Command command = Command::textCommand;
+        Opcode opcode;
+        uint8_t lengthHi, lengthLo; // Payload length = (256 * lengthHi) + lengthLo
+    } head;
+    static_assert(sizeof(Header) == 4, "TextCommand header has to be 4 bytes long");
+
+    std::span<const std::byte> payloadSpan;
+};
+} // namespace msg
+
+template<typename Net>
+class WebLink {
+    websocket::WebSocket<Net> ws;
+    WebLinkId id;
+    bool sameEndian;
+    std::optional<size_t> logSink;
+    std::function<void(WebLinkEvent)> eventsHandler;
+
+public:
+    WebLink(typename Net::Socket&& socket, WebLinkId webLinkId, std::function<void(WebLinkEvent)> eventHandler)
+        : ws(std::move(socket)), id(webLinkId), eventsHandler(eventHandler) {
+        log::debug("New WebLink created with id:{}", id);
+
+        ws.onMessage([this](std::string_view text) {
+            log::debug("onMessage(text) :{}", text);
+            ws.write("This is my response");
+        });
+        ws.onMessage([this](std::span<const std::byte> data) {
+            switch (static_cast<Command>(data[0])) {
+            case Command::handshake:
+                sendCommand(msg::Ack{});
+                logSink = log::addSinks([this](std::string_view t) { sendCommand(msg::DebugLog(t)); });
+                sameEndian = (std::endian::native == std::endian::little && static_cast<JSEndian>(data[1]) == JSEndian::little) ||
+                             (std::endian::native == std::endian::big && static_cast<JSEndian>(data[1]) == JSEndian::big);
+                log::info("Endianness - platform:{}, client:{}, identical:{}", std::endian::native == std::endian::little ? "little" : "big",
+                          static_cast<JSEndian>(data[1]) == JSEndian::little ? "little" : "big", sameEndian);
+                eventsHandler(WebLinkEvent(WebLinkEvent::Code::linked, id));
+                break;
+            default: break;
+            }
+            log::infoHex("onMessage(binary) :", data);
+        });
+
+        ws.start();
+    }
+    WebLink(const WebLink&) = delete;
+    WebLink(WebLink&&) = delete;
+    
+
+    ~WebLink() {
+        log::debug("WebLink destructor");
+        if (logSink) log::removeSinks(logSink.value());
+    }
+
+private:
+    void sendCommand(auto message) { ws.write(message.header(), message.payload()); }
+};
+
+} // namespace webfront
---
+++ include/tooling/Logger.hpp
@@ -0,0 +1,49 @@
+/// @date 28/01/2022 16:51:42
+/// @author Ambroise Leclerc
+/// @brief Logging facilities
+#pragma once
+#include "../details/C++20Support.hpp"
+#include "HexDump.hpp"
+
+#include <chrono>
+#include <filesystem>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <string_view>
+
+namespace webfront {
+namespace log {
+
+using LogType = const uint8_t;
+constinit LogType Disabled = 0, Error = 1, Warn = 2, Info = 3, Debug = 4;
+static auto clogSink = [](std::string_view t) { std::clog << t << "\n"; };
+
+namespace {
+    using namespace std; using srcLoc = source_location;
+    inline static struct Sinks {
+        void operator()(string_view t) const { for (auto& s : sinks) if (s) s(t); }
+        vector<function<void(string_view)>> sinks;
+    } out;
+    inline static bool logTypeEnabled[5];
+    constexpr char toChar(LogType l) { return l == Debug ? 'D' : l == Warn ? 'W' : l == Error ? 'E' : 'I'; }
+    inline static void log(LogType l, string_view text) { out(format("[{}] {:%T} | {}", toChar(l), chrono::system_clock::now(), text));}
+    inline static void log(LogType l, string_view text, const srcLoc& s) {
+        out(format("[{}] {:%T} | {:16}:{:4} | {}", toChar(l), chrono::system_clock::now(), filesystem::path(s.file_name()).filename().string(), s.line(), text)); }
+    inline static void set(LogType logType, bool enabled) { logTypeEnabled[logType] = enabled; }
+    inline static bool is(LogType logType) { return logTypeEnabled[logType]; }
+    inline static void setLogLevel(LogType l) { set(Error, l >= Error); set(Warn, l >= Warn); set(Info, l >= Info); set(Debug, l >= Debug);}
+}
+template<typename... Ts> struct debug { debug(string_view fmt, Ts&&... ts, const srcLoc& l = srcLoc::current()) {if (is(Debug)) log(Debug, format(fmt, ts...), l);}};
+template<typename... Ts> debug(string_view, Ts&&...) -> debug<Ts...>;
+template<typename... Ts> struct error { error(string_view fmt, Ts&&... ts) { if (is(Error)) log(Error, format(fmt, ts...)); } };
+template<typename... Ts> error(string_view, Ts&&...) -> error<Ts...>;
+template<typename... Ts> struct warn { warn(string_view fmt, Ts&&... ts) { if (is(Warn)) log(Warn, format(fmt, ts...)); } };
+template<typename... Ts> warn(string_view, Ts&&...) -> warn<Ts...>;
+template<typename... Ts> struct info { info(string_view fmt, Ts&&... ts) { if (is(Info)) log(Info, format(fmt, ts...)); } };
+template<typename... Ts> info(string_view, Ts&&...) -> info<Ts...>;
+void infoHex(string_view text, auto container) { if (is(Info)) { log(Info, text); out(utils::hexDump(container)); }}
+auto addSinks(auto&&... ts) { (out.sinks.push_back(forward<decltype(ts)>(ts)), ...); return out.sinks.size() - 1; }
+void removeSinks(auto&&... sinkIds) { ((out.sinks[sinkIds] = nullptr), ...); }
+} // namespace log
+} // namespace webfront
