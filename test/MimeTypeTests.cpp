896aee054269a282aa3fc06cfff44f839a7fe236
Ambroise Leclerc <ambroise.leclerc@webfront.fr>
2023-02-15 23:08:33 +0100

    sync

--- test/MimeTypeTests.cpp
+++ test/MimeTypeTests.cpp
@@ -1,8 +1,10 @@
 #include <http/MimeType.hpp>
 
 #include <doctest/doctest.h>
+
+#include <string>
+
 using namespace webfront::http;
-using namespace std::literals;
 
 SCENARIO("MimeTypes") {
     REQUIRE(MimeType::fromExtension("jpg").type == MimeType::jpg);
@@ -10,12 +12,12 @@ SCENARIO("MimeTypes") {
     REQUIRE(MimeType::fromExtension("mjs").type == MimeType::js);
 
     REQUIRE(std::string(MimeType::fromExtension("html").toString()) == "text/html");
-    REQUIRE(std::string(MimeType::fromExtension("htm").toString()) == "text/html"s);
-    REQUIRE(std::string(MimeType::fromExtension("bozo").toString()) == "text/plain"s);
-    REQUIRE(std::string(MimeType::fromExtension("gif").toString()) == "image/gif"s);
+    REQUIRE(std::string(MimeType::fromExtension("htm").toString()) == "text/html");
+    REQUIRE(std::string(MimeType::fromExtension("bozo").toString()) == "text/plain");
+    REQUIRE(std::string(MimeType::fromExtension("gif").toString()) == "image/gif");
 
-    REQUIRE(std::string(MimeType("pdf").toString()) == "application/pdf"s);
-    REQUIRE(std::string(MimeType(".ico").toString()) == "image/x-icon"s);
-    REQUIRE(std::string(MimeType(".webp").toString()) == "image/webp"s);
-    REQUIRE(std::string(MimeType("webp").toString()) == "image/webp"s);
+    REQUIRE(std::string(MimeType("pdf").toString()) == "application/pdf");
+    REQUIRE(std::string(MimeType(".ico").toString()) == "image/x-icon");
+    REQUIRE(std::string(MimeType(".webp").toString()) == "image/webp");
+    REQUIRE(std::string(MimeType("webp").toString()) == "image/webp");
 }
