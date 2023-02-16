431f2f103e0fc6d8cdc3ea0f1aad78b3e156c87c
Ambroise Leclerc <ambroise.leclerc@webfront.fr>
2023-02-16 01:58:50 +0100

    bak

--- test/doctest_main.cpp
+++ test/doctest_main.cpp
@@ -1,31 +1,3 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
 #include <doctest/doctest.h>
-#include <tooling/Logger.hpp>
-
-using namespace webfront;
-
-SCENARIO("Logger status shared between translation units") {
-    std::list<std::string> logs;
-    auto testSink = [&logs](std::string_view text) { logs.emplace_back(text); };
-    auto sinkId = log::addSinks(testSink);
-
-    GIVEN("A counting sink") {
-        log::setLogLevel(log::Disabled);
-
-        log::info("info log 1");
-        log::debug("debug log 2");
-        log::warn("warn log 3");
-        log::error("error log 4");
-        REQUIRE(logs.size() == 0);
-    }
-
-    GIVEN("A counting sink") {
-        log::setLogLevel(log::Warn);
-
-        log::debug("debug log 5");
-        log::warn("warn log 6");
-        REQUIRE(logs.size() == 1);
-    }
-    log::removeSinks(sinkId);
-}
