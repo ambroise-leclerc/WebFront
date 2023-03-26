9fd5efd97275e1cf2335db56c7bf34b8e81d6fab
Ambroise Leclerc <github.com@physigenics.com>
2023-03-26 20:37:09 +0200

    Missing mime types

--- test/MimeTypeTests.cpp
+++ test/MimeTypeTests.cpp
@@ -10,6 +10,12 @@ SCENARIO("MimeTypes") {
     REQUIRE(MimeType::fromExtension("jpg").type == MimeType::jpg);
     REQUIRE(MimeType::fromExtension("js").type == MimeType::js);
     REQUIRE(MimeType::fromExtension("mjs").type == MimeType::js);
+    REQUIRE(MimeType::fromExtension("css").type == MimeType::css);
+    REQUIRE(MimeType::fromExtension("png").type == MimeType::png);
+    REQUIRE(MimeType::fromExtension("json").type == MimeType::json);
+    REQUIRE(MimeType::fromExtension("ttf").type == MimeType::ttf);
+    REQUIRE(MimeType::fromExtension("svg").type == MimeType::svg);
+    REQUIRE(MimeType::fromExtension("csv").type == MimeType::csv);
 
     REQUIRE(std::string(MimeType::fromExtension("html").toString()) == "text/html");
     REQUIRE(std::string(MimeType::fromExtension("htm").toString()) == "text/html");
