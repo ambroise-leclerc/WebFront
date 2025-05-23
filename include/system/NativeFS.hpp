6788a8e773de15f26f7da19165008cd7f2ef8983
Ambroise Leclerc <6860842+ambroise-leclerc@users.noreply.github.com>
2025-05-23 09:12:23 +0000

    Comment now corresponds to the class

--- include/system/NativeFS.hpp
+++ include/system/NativeFS.hpp
@@ -1,15 +1,15 @@
 /// @date 26/04/2023 16:39:42
 /// @author Ambroise Leclerc
-/// @brief Virtual file system providing access to Jasmine Javascript testing framework.
+/// @brief Native file system providing direct access to the operating system's file system.
 #pragma once
 
 #include "FileSystem.hpp"
-
 #include <fstream>
 
 namespace webfront::fs {
 
 namespace detail {
+/// @brief Low-level implementation of a native file system.
 class NativeRawFS {
 public:
     std::optional<File> open(std::filesystem::path path) {
@@ -18,17 +18,17 @@ public:
         if (file.is_open()) return File{std::move(file)};
         return {};
     }
-
 protected:
     std::filesystem::path rootPath;
 };
-} // namespace
+} // namespace detail
 
+/// @brief Native file system for debugging purposes.
 class NativeDebugFS : public detail::NativeRawFS {
 public:
-    NativeDebugFS(std::filesystem::path docRoot) {
+    explicit NativeDebugFS(std::filesystem::path docRoot) {
         rootPath = docRoot;
     }
-
 };
+
 } // namespace webfront::fs
